215300a2e332d709aec67093c271be0be11faf96
Axent13 <axent13@yandex.ru>
2020-03-04 20:37:26 +0700

    Добавил webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   module: {
     rules: [
