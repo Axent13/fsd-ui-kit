3b033b632232b6ae5eb1052c3df878ade075230a
Axent13 <axent13@yandex.ru>
2020-05-08 20:32:11 +0700

    Добавлена функция склонения числительных

--- src/components/item-quantity-dropdown/item-quantity-dropdown.js
+++ src/components/item-quantity-dropdown/item-quantity-dropdown.js
@@ -1,3 +1,36 @@
+const options = {
+  placeholder: 'Сколько гостей',
+  options: [
+    {
+      name: 'спальни',
+      form1: 'спальня',
+      form2: 'спальни',
+      form5: 'спален',
+    },
+    {
+      name: 'кровати',
+      form1: 'кровать',
+      form2: 'кровати',
+      form5: 'кроватей',
+    },
+    {
+      name: 'ванные комнаты',
+      form1: 'ванная комната',
+      form2: 'ванных комнаты',
+      form5: 'ванных комнат',
+    },
+  ],
+};
+
+function pluralForm(qauntity, form1, form2, form5) {
+  const reminderOfTheHundred = Math.abs(qauntity) % 100;
+  const reminderOfTheTen = reminderOfTheHundred % 10;
+  if (reminderOfTheHundred > 10 && reminderOfTheHundred < 20) return form5;
+  if (reminderOfTheTen > 1 && reminderOfTheTen < 5) return form2;
+  if (reminderOfTheTen === 1) return form1;
+  return form5;
+}
+
 const $select = $('.js-item-quantity-dropdown__select');
 const $optionsContainer = $('.js-item-quantity-dropdown__options-container');
 
@@ -10,3 +43,6 @@ function initEventListeners() {
 }
 
 initEventListeners();
+
+const test = options.options[0];
+console.log(pluralForm(111122, test.form1, test.form2, test.form5));
