86bbc30bf54b42e8cf9d28776c7697069f69c7d1
Axent13 <axent13@yandex.ru>
2020-05-29 13:20:35 +0700

    Date-dropdown:подключил air datepicker

---
+++ src/components/date-dropdown/date-dropdown.js
@@ -0,0 +1,6 @@
+import 'air-datepicker';
+import 'air-datepicker/dist/css/datepicker.css';
+
+const $dateDropdown = $('.js-air-datepicker');
+
+$dateDropdown.datepicker();
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: [
           'style-loader',
           'css-loader',
