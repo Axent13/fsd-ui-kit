754d310c061b0d5408ab2c7e21b66f81f826941e
Axent13 <axent13@yandex.ru>
2020-06-17 19:31:58 +0700

    Сделал date-dropdown

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,6 +1,11 @@
 import 'air-datepicker';
 import 'air-datepicker/dist/css/datepicker.css';
 
-const $dateDropdown = $('.js-air-datepicker');
+const $dateDropdown = $('.js-date-dropdown');
+const $airDatepicker = $('.js-date-dropdown__air-datepicker');
 
-$dateDropdown.datepicker();
+$airDatepicker.datepicker();
+console.log($airDatepicker.data('datepicker'));
+
+$dateDropdown.innerHTML = $airDatepicker.data('datepicker').currentDate;
+console.log($airDatepicker.data('datepicker').currentDate);
