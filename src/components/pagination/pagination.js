277b10c034652946b2c1cf9203ff208fbbda20b6
Axent13 <axent13@yandex.ru>
2020-07-16 07:28:02 +0700

    pagination: логика без обработки нажатия кнопок

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,47 +1,86 @@
 const dataFromSomewhere = {
   currentPage: 1,
   totalPages: 15,
-  paginationLength: 6,
+  paginationLengthLeft: 2,
+  paginationLengthRight: 1,
 };
 
-const { currentPage } = dataFromSomewhere;
-const { totalPages } = dataFromSomewhere;
-const { paginationLength } = dataFromSomewhere;
-// const $paginationRootElement = document.querySelector('.pagination');
-const $paginationButtonsContainer = document.querySelector('.pagination__buttons');
-let pageIndex = 0;
+function drawPagination(options) {
+  const { currentPage } = options;
+  let pageIndex = currentPage;
+  const { totalPages } = options;
+  const { paginationLengthLeft } = options;
+  const { paginationLengthRight } = options;
+  const $paginationButtonsContainer = $('.pagination__buttons');
 
-// Добавлем первую кнопку
-const $newFirstButton = document.createElement('button');
-if (currentPage === 1) {
-  $newFirstButton.classList.add('pagination__page-button', 'pagination__page-button_active');
-  $newFirstButton.innerText = '1';
-  pageIndex += 1;
-} else {
-  $newFirstButton.classList.add('pagination__arrow-button');
-  $newFirstButton.innerText = '<-';
-}
-$paginationButtonsContainer.appendChild($newFirstButton);
-
-// Добавляем промежуточные кнопки
-for (; pageIndex < paginationLength - 2; pageIndex += 1) {
-  const $newPageButton = document.createElement('button');
-
-  $newPageButton.classList.add('pagination__page-button');
-  $newPageButton.innerText = currentPage + pageIndex;
-  if (currentPage === pageIndex) {
-    $newPageButton.classList.add('pagination__page-button_active');
+  // Добавлем первую кнопку
+  const $newFirstButton = document.createElement('button');
+  if (currentPage === 1) {
+    $newFirstButton.classList.add('pagination__page-button', 'pagination__page-button_active');
+    $newFirstButton.innerText = '1';
+    pageIndex += 1;
+  } else {
+    $newFirstButton.classList.add('pagination__arrow-button');
+    $newFirstButton.innerText = '<-';
   }
-  $paginationButtonsContainer.appendChild($newPageButton);
+  $paginationButtonsContainer.append($newFirstButton);
+
+  // Добавляем левую часть
+  for (let lengthIndex = 0; lengthIndex < paginationLengthLeft; lengthIndex += 1) {
+    // Если страницы закончились - досрочно выходим из цикла
+    if (pageIndex + 1 > totalPages) {
+      break;
+    }
+    const $newPageButton = document.createElement('button');
+    $newPageButton.classList.add('pagination__page-button');
+    $newPageButton.innerText = pageIndex;
+    if (currentPage === pageIndex) {
+      $newPageButton.classList.add('pagination__page-button_active');
+    }
+    $paginationButtonsContainer.append($newPageButton);
+    pageIndex += 1;
+  }
+
+  // Если не все страницы будут отрисованы, то добавляем многоточие
+  if (pageIndex + paginationLengthRight <= totalPages) {
+    const $newRestButton = document.createElement('button');
+    $newRestButton.classList.add('pagination__page-button');
+    // Если всего лишь одна страница не влезла, то вместо многоточия пишем её номер
+    if (pageIndex === totalPages - 1) {
+      $newRestButton.innerText = pageIndex;
+    } else {
+      $newRestButton.innerText = '...';
+    }
+    $paginationButtonsContainer.append($newRestButton);
+  }
+
+  // Добавляем правую часть
+  if (pageIndex <= totalPages) {
+    // Начинаем отсчет с нужного номера страницы
+    pageIndex = totalPages - paginationLengthRight + 1;
+    for (let lengthIndex = 0; lengthIndex < paginationLengthRight; lengthIndex += 1) {
+      // Если страницы закончились - досрочно выходим из цикла
+      if (pageIndex > totalPages) {
+        break;
+      }
+      const $newPageButton = document.createElement('button');
+      $newPageButton.classList.add('pagination__page-button');
+      $newPageButton.innerText = pageIndex;
+      if (currentPage === pageIndex) {
+        $newPageButton.classList.add('pagination__page-button_active');
+      }
+      $paginationButtonsContainer.append($newPageButton);
+      pageIndex += 1;
+    }
+  }
+
+  // Добавляем стрелку справа, если эта страница не последняя
+  if (currentPage < totalPages) {
+    const $newLastButton = document.createElement('button');
+    $newLastButton.classList.add('pagination__arrow-button');
+    $newLastButton.innerText = '->';
+    $paginationButtonsContainer.append($newLastButton);
+  }
 }
 
-// Добавляем последнюю кнопку
-const $newLastButton = document.createElement('button');
-if (currentPage === totalPages) {
-  $newLastButton.classList.add('pagination__page-button', 'pagination__page-button_active');
-  $newLastButton.innerText = totalPages;
-} else {
-  $newLastButton.classList.add('pagination__arrow-button');
-  $newLastButton.innerText = '->';
-}
-$paginationButtonsContainer.appendChild($newLastButton);
+drawPagination(dataFromSomewhere);
