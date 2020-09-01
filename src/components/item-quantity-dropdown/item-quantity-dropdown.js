ff2f14257447ce787aa657da6d9d2e52809ee65c
Axent13 <axent13@yandex.ru>
2020-09-01 21:37:27 +0700

    Сделал стартовую страницу

--- src/components/item-quantity-dropdown/item-quantity-dropdown.js
+++ src/components/item-quantity-dropdown/item-quantity-dropdown.js
@@ -80,7 +80,6 @@ class ItemQuantityDropdown {
         if (newCount > 0) {
           $newDecrement.removeClass('item-quantity-dropdown__decrement_disabled');
         }
-
       }
       $newIncrement.on('click', handleIncrementClick);
 
@@ -106,11 +105,6 @@ class ItemQuantityDropdown {
     $clearAndApplyButtonsContainer.append($clearButton);
     $clearAndApplyButtonsContainer.append($applyButton);
     this.$options.append($clearAndApplyButtonsContainer);
-  }
-
-  _updateValue() {
-    /* Нужно перебрать в цикле все созданные options'ы и записать значения из options объекта optinsDatFromSomewhere*/
-    // this.$select.innerHTML = 
   }
 }
 
