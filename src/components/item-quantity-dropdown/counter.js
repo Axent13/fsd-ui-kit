438fe0f66a9b923f3392ffd59b190e7f1fd103a8
Axent13 <axent13@yandex.ru>
2020-09-06 14:56:03 +0700

    IQ-dropdown: отображение текста

--- src/components/item-quantity-dropdown/counter.js
+++ src/components/item-quantity-dropdown/counter.js
@@ -4,30 +4,7 @@ class Counter {
     this.$decrement = $(`<button class="${options.decrementClasses}" type="button">-</button>`);
     this.$counter = $(`<div class="${options.counterClasses}">${this.count}</div>`);
     this.$increment = $(`<button class="${options.incrementClasses}" type="button">+</button>`);
-    this.initEventListeners();
-  }
-
-  handleDecrementClick() {
-    this.count = parseInt($(this.$counter).text(), 10) - 1;
-    if (this.count >= 0) {
-      $(this.$counter).text(this.count);
-    }
-    if (this.count === 0) {
-      this.$decrement.addClass('item-quantity-dropdown__decrement_disabled');
-    }
-  }
-
-  handleIncrementClick() {
-    this.count = parseInt($(this.$counter).text(), 10) + 1;
-    $(this.$counter).text(this.count);
-    if (this.count > 0) {
-      this.$decrement.removeClass('item-quantity-dropdown__decrement_disabled');
-    }
-  }
-
-  initEventListeners() {
-    this.$decrement.on('click', this.handleDecrementClick.bind(this));
-    this.$increment.on('click', this.handleIncrementClick.bind(this));
+    this._initEventListeners();
   }
 
   getDecrementElement() {
@@ -40,6 +17,34 @@ class Counter {
 
   getIncrementElement() {
     return this.$increment;
+  }
+
+  getCounterValue() {
+    return this.count;
+  }
+
+  _handleDecrementClick() {
+    this.count = parseInt($(this.$counter).text(), 10);
+    if (this.count > 0) {
+      this.count -= 1;
+      $(this.$counter).text(this.count);
+    }
+    if (this.count === 0) {
+      this.$decrement.addClass('item-quantity-dropdown__decrement_disabled');
+    }
+  }
+
+  _handleIncrementClick() {
+    this.count = parseInt($(this.$counter).text(), 10) + 1;
+    $(this.$counter).text(this.count);
+    if (this.count > 0) {
+      this.$decrement.removeClass('item-quantity-dropdown__decrement_disabled');
+    }
+  }
+
+  _initEventListeners() {
+    this.$decrement.on('click', this._handleDecrementClick.bind(this));
+    this.$increment.on('click', this._handleIncrementClick.bind(this));
   }
 }
 
--- src/components/item-quantity-dropdown/item-quantity-dropdown.js
+++ src/components/item-quantity-dropdown/item-quantity-dropdown.js
@@ -1,51 +1,155 @@
 import Counter from './counter';
 
-const optionsDataFromSomewhere = {
-  placeholder: 'Удобства номера',
-  hasClearAndApplyButtons: true,
-  options: [
-    {
-      name: 'спальни',
-      form1: 'спальня',
-      form2: 'спальни',
-      form5: 'спален',
-    },
-    {
-      name: 'кровати',
-      form1: 'кровать',
-      form2: 'кровати',
-      form5: 'кроватей',
-    },
-    {
-      name: 'ванные комнаты',
-      form1: 'ванная комната',
-      form2: 'ванных комнаты',
-      form5: 'ванных комнат',
-    },
-  ],
-};
-
-// function pluralForm(qauntity, form1, form2, form5) {
-//   const reminderOfTheHundred = Math.abs(qauntity) % 100;
-//   const reminderOfTheTen = reminderOfTheHundred % 10;
-//   if (reminderOfTheHundred > 10 && reminderOfTheHundred < 20) return form5;
-//   if (reminderOfTheTen > 1 && reminderOfTheTen < 5) return form2;
-//   if (reminderOfTheTen === 1) return form1;
-//   return form5;
-// }
-
+const optionsDataFromSomewhere = [
+  {
+    placeholder: 'Сколько гостей',
+    hasClearAndApplyButtons: true,
+    maxTextLength: 40,
+    options: [
+      {
+        name: 'взрослые',
+        form1: 'взрослый',
+        form2: 'взрослых',
+        form5: 'взрослых',
+      },
+      {
+        name: 'дети',
+        form1: 'ребенок',
+        form2: 'ребенка',
+        form5: 'детей',
+      },
+      {
+        name: 'младенцы',
+        form1: 'младенец',
+        form2: 'младенца',
+        form5: 'младенцев',
+      },
+    ],
+  },
+  {
+    placeholder: 'Удобства номера',
+    hasClearAndApplyButtons: true,
+    maxTextLength: 25,
+    options: [
+      {
+        name: 'спальни',
+        form1: 'спальня',
+        form2: 'спальни',
+        form5: 'спален',
+      },
+      {
+        name: 'кровати',
+        form1: 'кровать',
+        form2: 'кровати',
+        form5: 'кроватей',
+      },
+      {
+        name: 'ванные комнаты',
+        form1: 'ванная комната',
+        form2: 'ванных комнаты',
+        form5: 'ванных комнат',
+      },
+    ],
+  },
+  {
+    placeholder: 'Удобства номера',
+    hasClearAndApplyButtons: true,
+    maxTextLength: 25,
+    options: [
+      {
+        name: 'спальни',
+        form1: 'спальня',
+        form2: 'спальни',
+        form5: 'спален',
+      },
+      {
+        name: 'кровати',
+        form1: 'кровать',
+        form2: 'кровати',
+        form5: 'кроватей',
+      },
+      {
+        name: 'ванные комнаты',
+        form1: 'ванная комната',
+        form2: 'ванных комнаты',
+        form5: 'ванных комнат',
+      },
+    ],
+  },
+  {
+    placeholder: 'Сколько гостей',
+    hasClearAndApplyButtons: true,
+    maxTextLength: 40,
+    options: [
+      {
+        name: 'взрослые',
+        form1: 'взрослый',
+        form2: 'взрослых',
+        form5: 'взрослых',
+      },
+      {
+        name: 'дети',
+        form1: 'ребенок',
+        form2: 'ребенка',
+        form5: 'детей',
+      },
+      {
+        name: 'младенцы',
+        form1: 'младенец',
+        form2: 'младенца',
+        form5: 'младенцев',
+      },
+    ],
+  },
+  {
+    placeholder: 'Сколько гостей',
+    hasClearAndApplyButtons: true,
+    maxTextLength: 40,
+    options: [
+      {
+        name: 'взрослые',
+        form1: 'взрослый',
+        form2: 'взрослых',
+        form5: 'взрослых',
+      },
+      {
+        name: 'дети',
+        form1: 'ребенок',
+        form2: 'ребенка',
+        form5: 'детей',
+      },
+      {
+        name: 'младенцы',
+        form1: 'младенец',
+        form2: 'младенца',
+        form5: 'младенцев',
+      },
+    ],
+  },
+];
 
 class ItemQuantityDropdown {
   constructor($rootElement, options = {}) {
+    this.options = options;
+    this.maxTextLength = this.options.maxTextLength || 25;
+    this.counters = [];
     this.$rootElement = $rootElement;
     this.$select = this.$rootElement.find('.js-item-quantity-dropdown__select');
     this.$arrow = this.$rootElement.find('.js-item-quantity-dropdown__arrow');
     this.$options = this.$rootElement.find('.js-item-quantity-dropdown__options');
+
+    this._createOptions(options);
+    this.$clearAndApplyButtonsContainer = $('<div class="item-quantity-dropdown__clear-and-apply-buttons-container"></div>');
+    this.$clearButton = $('<div class="item-quantity-dropdown__clear item-quantity-dropdown__clear_disabled">Очистить</div>');
+    this.$applyButton = $('<div class="item-quantity-dropdown__apply">Применить</div>');
+    this.$clearAndApplyButtonsContainer.append(this.$clearButton);
+    this.$clearAndApplyButtonsContainer.append(this.$applyButton);
+    this.$options.append(this.$clearAndApplyButtonsContainer);
+
     this._handleSelectClick = this._handleSelectClick.bind(this);
+    this._handleApllyButtonClick = this._handleApllyButtonClick.bind(this);
     this._initEventListeners = this._initEventListeners.bind(this);
     this._initEventListeners();
-    this._createOptions(options);
-    this._createClearAndApplyButtons();
   }
 
   _handleSelectClick() {
@@ -54,8 +158,52 @@ class ItemQuantityDropdown {
     this.$options.toggleClass('item-quantity-dropdown__options_expanded');
   }
 
+  _handleApllyButtonClick() {
+    const values = [];
+    let resultText = '';
+    this.counters.forEach((currentCounter) => {
+      values.push(currentCounter.getCounterValue());
+    });
+
+    this.options.options.forEach((option, index) => {
+      if (values[index] !== 0) {
+        const optionName = ItemQuantityDropdown.pluralForm(
+          values[index],
+          option.form1,
+          option.form2,
+          option.form5,
+        );
+
+        resultText += `${values[index]}  ${optionName}, `;
+      }
+    });
+    // убираю лишние символы "пробела" и "запятой" после последнего элемента
+    resultText = resultText.slice(0, -2);
+    // обрезаю текст, при превышении максимальной длины текста
+    if (resultText.length > this.maxTextLength) {
+      resultText = resultText.slice(0, this.maxTextLength - 3);
+      resultText += '...';
+    }
+    // но если везде был указан 0 - возвращаю placeholder
+    if (resultText === '') {
+      resultText = this.options.placeholder;
+    }
+
+    $(this.$select).text(resultText);
+  }
+
+  static pluralForm(qauntity, form1, form2, form5) {
+    const reminderOfTheHundred = Math.abs(qauntity) % 100;
+    const reminderOfTheTen = reminderOfTheHundred % 10;
+    if (reminderOfTheHundred > 10 && reminderOfTheHundred < 20) return form5;
+    if (reminderOfTheTen > 1 && reminderOfTheTen < 5) return form2;
+    if (reminderOfTheTen === 1) return form1;
+    return form5;
+  }
+
   _initEventListeners() {
     this.$select.on('click', this._handleSelectClick);
+    this.$applyButton.on('click', this._handleApllyButtonClick);
   }
 
   _createOptions(optionsData) {
@@ -69,6 +217,7 @@ class ItemQuantityDropdown {
         counterClasses: 'item-quantity-dropdown__counter',
         incrementClasses: 'item-quantity-dropdown__increment',
       });
+      this.counters.push($newCounter);
 
       $newCounterContainer.append($newCounter.getDecrementElement());
       $newCounterContainer.append($newCounter.getCounterElement());
@@ -78,16 +227,6 @@ class ItemQuantityDropdown {
       this.$options.append($newOption);
     });
   }
-
-  _createClearAndApplyButtons() {
-    const $clearAndApplyButtonsContainer = $('<div class="item-quantity-dropdown__clear-and-apply-buttons-container"></div>');
-    const $clearButton = $('<div class="item-quantity-dropdown__clear item-quantity-dropdown__clear_disabled">Очистить</div>');
-    const $applyButton = $('<div class="item-quantity-dropdown__apply">Применить</div>');
-
-    $clearAndApplyButtonsContainer.append($clearButton);
-    $clearAndApplyButtonsContainer.append($applyButton);
-    this.$options.append($clearAndApplyButtonsContainer);
-  }
 }
 
 // Находим все элементы на странице с таким классом
@@ -95,5 +234,5 @@ const $rootElements = $('.js-item-quantity-dropdown');
 
 // В цикле для каждого элемента создаём объект
 $rootElements.each((index, node) => {
-  new ItemQuantityDropdown($(node), optionsDataFromSomewhere);
+  new ItemQuantityDropdown($(node), optionsDataFromSomewhere[index]);
 });
