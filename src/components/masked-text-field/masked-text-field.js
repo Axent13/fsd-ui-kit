73054023772b4e9c2ccf37c641cb798e1fa84d9f
Axent13 <axent13@yandex.ru>
2020-09-25 22:21:48 +0700

    Masked-text-field: переписал на классах

--- src/components/masked-text-field/masked-text-field.js
+++ src/components/masked-text-field/masked-text-field.js
@@ -1,3 +1,15 @@
 import Inputmask from 'inputmask';
 
-Inputmask('99.99.9999').mask('.js-masked-text-field__input');
+class MaskedTextField {
+  constructor($rootElement) {
+    this.$rootElement = $rootElement;
+    this.$input = this.$rootElement.find('.js-masked-text-field__input');
+    Inputmask('99.99.9999').mask(this.$input);
+  }
+}
+
+const $rootElements = $('.js-masked-text-field');
+
+$rootElements.each((index, node) => {
+  new MaskedTextField($(node));
+});
