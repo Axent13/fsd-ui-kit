3fdb0562c559374e7d445cb654f17fd3fbec0ed6
Axent13 <Axent13@yandex.ru>
2022-09-19 15:06:14 +0700

    Исправил загрузку iq-dropdown'а в card-booking

--- src/pages/cards/cards-data.js
+++ src/pages/cards/cards-data.js
@@ -11,6 +11,46 @@ const cardsData = {
       dailyPrice: 9990,
     },
   ],
+  bookingData: [
+    {
+      placeholder: 'Сколько гостей',
+      hasClearAndApplyButtons: false,
+      maxTextLength: 40,
+      options: [
+        {
+          optionsNames: ['взрослые', 'дети'],
+          form1: 'гость',
+          form2: 'гостя',
+          form5: 'гостей',
+        },
+        {
+          optionsNames: ['младенцы'],
+          form1: 'младенец',
+          form2: 'младенца',
+          form5: 'младенцев',
+        },
+      ],
+    },
+    {
+      placeholder: 'Сколько гостей',
+      hasClearAndApplyButtons: false,
+      maxTextLength: 40,
+      options: [
+        {
+          optionsNames: ['взрослые', 'дети'],
+          form1: 'гость',
+          form2: 'гостя',
+          form5: 'гостей',
+        },
+        {
+          optionsNames: ['младенцы'],
+          form1: 'младенец',
+          form2: 'младенца',
+          form5: 'младенцев',
+        },
+      ],
+    },
+  ]
 };
 
 export default cardsData;
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,11 +1,13 @@
 import DateDropdown from '../../components/date-dropdown/date-dropdown';
 import RoomCard from '../../components/room-card/room-card';
 import cardsData from './cards-data';
+import ItemQuantityDropdown from '../../components/item-quantity-dropdown/item-quantity-dropdown';
 
 $(document).ready(() => {
   const $cardsRootElement = $('.js-cards');
   const $dateDropdownElements = $cardsRootElement.find('.js-date-dropdown');
   const $roomCardsElements = $cardsRootElement.find('.js-cards__room-card-container');
+  const $itemQuantityDropdownElements = $cardsRootElement.find('.js-item-quantity-dropdown');
 
   $dateDropdownElements.each((index, node) => {
     new DateDropdown($(node), {
@@ -16,6 +18,10 @@ $(document).ready(() => {
         days: 'MM <i>yyyy</i>',
       },
     });
+  });
+
+  $itemQuantityDropdownElements.each((index, node) => {
+    new ItemQuantityDropdown($(node), cardsData.bookingData[index]);
   });
 
   $roomCardsElements.each((index, node) => {
